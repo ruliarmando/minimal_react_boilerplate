8e4191b2745048ff987abcd92068314a81b57e6a
Rully Ramanda <krooco@gmail.com>
2018-02-14 20:55:58 +0700

    First commit :tada: :tada:

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.css';
+
+const App = () => (
+  <div className={styles.app}>
+    <h2>My Minimal React Webpack Babel Boilerplate!</h2>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader!babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader?parser=postcss-scss'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
